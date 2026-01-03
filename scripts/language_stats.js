f52a078fb8ffeeb84c23e79527b28b9300f11ea1
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-01-03 14:40:13 +0100

    Generate svg

--- scripts/language_stats.js
+++ scripts/language_stats.js
@@ -46,11 +46,15 @@ const res = await fetch("https://api.github.com/graphql", {
 });
 
 const json = await res.json()
+if (json.errors) throw json.errors;
+
 const repos = json.data.user.repositoriesContributedTo.nodes;
 
 // Collect all languages and their size
 const totals = {}
 repos.forEach(repo => {
+    if (!repo?.languages?.edges) return; // If it cant be accessed, doesnt exists, etc.
+
     repo.languages.edges.forEach(({ size, node}) => {
         totals[node.name] = (totals[node.name] || 0) + size;
     });
@@ -59,9 +63,32 @@ repos.forEach(repo => {
 // Sort languages by size
 const sorted = Object.entries(totals)
     .sort((a, b) => b[1] - a[1]) // Bytes for language B - bytes for language a -> largest first
-    .slice(0, 6); // Keep top 6
+    .slice(0, 10); // Keep top 10
 
 // Get total size of all langs in bytes
 const sum = sorted.reduce((a, [, v]) => a + v, 0)
 
+let y = 30;
+const bars = sorted.map(([lang, size]) => {
+    const pct = ((size / sum) * 100).toFixed(1);
+    const bar_width = Math.round((size / sum) * 260);
+    const row = `
+    <text x="0" y ="${y + 12}" fill="#c9d1d9" font-size="12">${lang} ${pct}%</text>
+    <rect x="0" y="${y + 16}" width="${bar_width}" height="8" fill="#58a6ff" />
+    `;
+    y += 32;
+    return row
+}).join("");
 
+const svg = `
+<svg width="260" height="${y}" viewBox="0 0 260 ${y}"
+    xmlns="http://www.w3.org/2000/svg">
+    <style>
+        text { font-family: system-ui, -apple-system, BlinkMacSystemFont; }
+    </style>
+    ${bars}
+</svg>
+`;
+
+fs.mkdirSync("assets", { recursive: true });
+fs.writeFileSync("assets/top-lang.svg", svg.trim());
