4d2518ba7609fdab8eca16b68cb3b22c3d0666a5
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-01-04 16:02:01 +0100

    Repo line counts

--- scripts/count_lines.js
+++ scripts/count_lines.js
@@ -1,8 +1,8 @@
 import fs from "fs";
 import { execSync } from "child_process";
 
-// const user = process.env.GH_USER;
-const user = "I-had-a-bad-idea";
+const user = process.env.GH_USER;
+// const user = "I-had-a-bad-idea";
 
 let all_repos = {};
 
@@ -19,13 +19,13 @@ const active_repos = active_repos_json.data.user.repositories.nodes;
 
 function clone_all_repos(repos) {
     repos.forEach(repo => {
-        execSync(`git clone ${repo.url} repos/${repo.name}`);
+        execSync(`git clone ${repo.url} repos/${repo.name}`, { stdio: 'inherit' });
     })
 }
 
 function count_lines_in_repo(repo_name) {
 
-    execSync(`cloc --json repos/${repo_name} > temp/${repo_name}.json`);
+    execSync(`cloc --json repos/${repo_name} > temp/${repo_name}.json`, { stdio: 'inherit' });
 
     let langs = {};
     const langs_in_repo = JSON.parse(fs.readFileSync(`temp/${repo_name}.json`));
@@ -40,7 +40,7 @@ function count_lines_in_repo(repo_name) {
 
 function count_commited_lines_in_repo(repo_name) {
     const cmd = `git -C repos/${repo_name} log --author "${user}" --pretty=tformat: --numstat`;
-    const output = execSync(cmd, { encoding: "utf-8" });
+    const output = execSync(cmd, { encoding: "utf-8", stdio: `pipe` });
 
     let added = 0;
     let removed = 0;
@@ -57,10 +57,14 @@ function count_commited_lines_in_repo(repo_name) {
 
 fs.mkdirSync("repos", { recursive: true });
 fs.mkdirSync("temp", { recursive: true });
-clone_all_repos(active_repos);
+// clone_all_repos(active_repos);
 for (let i = 0; i < active_repos.length; i++) {
     let repo_name = active_repos[i].name;
-    all_repos[repo_name] = [count_lines_in_repo(repo_name)], count_commited_lines_in_repo(repo_name);
+    console.log(`Doing repo: ${repo_name}`);
+    all_repos[repo_name] = [
+        count_lines_in_repo(repo_name),
+        count_commited_lines_in_repo(repo_name)
+    ];
 };
 
 fs.writeFileSync("assets/repo-line-counts.txt", JSON.stringify(all_repos, null, 2));
