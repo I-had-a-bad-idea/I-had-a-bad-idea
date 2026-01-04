e7eed44477a9fc2d32b980be73495a2695f6b8ae
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-01-04 15:29:57 +0100

    Count committed lines

--- scripts/count_lines.js
+++ scripts/count_lines.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 import { execSync } from "child_process";
 
+// const user = process.env.GH_USER;
+const user = "I-had-a-bad-idea";
+
 let all_repos = {};
 
 if (fs.existsSync("assets/repo-line-counts.txt")) {
@@ -14,15 +17,19 @@ const active_repos_json = JSON.parse(active_repos_data);
 
 const active_repos = active_repos_json.data.user.repositories.nodes;
 
-function count_lines_in_repo(repo_name, repo_url) {
-    execSync(`git clone ${repo_url} repos/${repo_name}`);
+function clone_all_repos(repos) {
+    repos.forEach(repo => {
+        execSync(`git clone ${repo.url} repos/${repo.name}`);
+    })
+}
+
+function count_lines_in_repo(repo_name) {
 
     execSync(`cloc --json repos/${repo_name} > temp/${repo_name}.json`);
 
     let langs = {};
     const langs_in_repo = JSON.parse(fs.readFileSync(`temp/${repo_name}.json`));
 
-    // fs.writeFileSync("temp/lines", JSON.stringify(langs_in_repo, null, 2));
     for (const [lang, status] of Object.entries(langs_in_repo)) {
         if (!status.code) continue;
         langs[lang] = status.code;
@@ -31,11 +38,29 @@ function count_lines_in_repo(repo_name, repo_url) {
     return langs;
 }
 
+function count_commited_lines_in_repo(repo_name) {
+    const cmd = `git -C repos/${repo_name} log --author "${user}" --pretty=tformat: --numstat`;
+    const output = execSync(cmd, { encoding: "utf-8" });
+
+    let added = 0;
+    let removed = 0;
+
+    output.split("\n").forEach(line => {
+        const parts = line.trim().split("\t");
+        if (parts.length < 2) return;
+        added += parseInt(parts[0]) || 0;
+        removed += parseInt(parts[1]) || 0;
+    })
+
+    return [added, removed];
+}
+
 fs.mkdirSync("repos", { recursive: true });
 fs.mkdirSync("temp", { recursive: true });
-active_repos.forEach(active_repo => {
-    all_repos[active_repo.name] = count_lines_in_repo(active_repo.name, active_repo.url);
-});
-
+clone_all_repos(active_repos);
+for (let i = 0; i < active_repos.length; i++) {
+    let repo_name = active_repos[i].name;
+    all_repos[repo_name] = [count_lines_in_repo(repo_name)], count_commited_lines_in_repo(repo_name);
+};
 
 fs.writeFileSync("assets/repo-line-counts.txt", JSON.stringify(all_repos, null, 2));
